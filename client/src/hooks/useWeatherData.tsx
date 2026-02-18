792800ede4a7b13adf650c39bcef00bcec0bb196
Nick Toye <coding.toye@gmail.com>
2026-02-18 23:12:04 +0000

    Update API base URL for weather data

--- client/src/hooks/useWeatherData.tsx
+++ client/src/hooks/useWeatherData.tsx
@@ -5,7 +5,7 @@ import {useState, useCallback} from "react";
 import {SearchedLocation} from "../types/types";
 
 const BASE_URL =
-  "https://us-central1-weather-dashboard-417915.cloudfunctions.net/api";
+  "https://weather-dashboard-417915.web.app/.cloudfunctions.net/api";
 
 const defaultWeatherData: SearchedLocation = {
   current: {
